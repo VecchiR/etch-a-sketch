4da688144ada780f2cc05391ce008374ebc6d966
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-01-24 20:44:00 -0300

    Add functions to change square color and reset
    Add event listeners and functions to:
    -> Change square color when mouse hovers over it;
    -> Reset all squares' colors when reset button is clicked OR key 'r'
    is pressed.
    
    Also: remove borders from squares + minor changes in CSS, like colors
    used.

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const gridContainer = document.querySelector('#grid-container');
 
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', resetGrid);
+
+addEventListener('keydown', (e) => {
+    e.key === 'r' ? resetGrid() : void(0);
+});
+
+
 for (i = 0; i < 256; i++) {
     const sqrDiv = document.createElement('div');
     sqrDiv.className = 'square-div';
     gridContainer.appendChild(sqrDiv);
+    sqrDiv.addEventListener('mouseenter', () => {
+        sqrDiv.style.backgroundColor = 'black';
+    })
 }
 
+function resetGrid() {
+    const allSquares = document.querySelectorAll('.square-div');
 
-// const gridContainer = document.querySelector('.flex-container');
-
-// for (i=0; i<64; i++) {
-//     const sqrDiv = document.createElement('div');
-//     sqrDiv.className = 'square-div';
-//     gridContainer.appendChild(sqrDiv);
-// }
+    allSquares.forEach((square) => {
+        square.style.backgroundColor = '';
+    })
+}
